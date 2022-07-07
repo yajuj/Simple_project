4ff1a970fa9d661f2baac3b03bed59f5608a7096
yajuj <noticethatred@gmail.com>
2022-07-07 07:03:38 +0500

    deleted unused js

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Models\Book;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::redirect('/', '/index');
+
+Route::fallback(function () {
+  return abort(404);
+});
 
 Route::namespace('Material')->prefix('index')->group(function () {
   Route::get('/create', CreateController::class)->name('create-material');
